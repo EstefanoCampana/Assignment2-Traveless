852f1ae0225e08d231dd6026c476dd90ee65e1fc
Estefano Campana <Estefano.CampanaMunoz@edu.sait.ca>
2025-02-28 09:12:40 -0700

    Updates reservations class

--- Data/Reservations.cs
+++ Data/Reservations.cs
@@ -10,5 +10,11 @@ namespace BlazorHybridApp.Data
     {
         //Private data
         private string rcode = String.Empty;
+        private string fcode = String.Empty;
+        private string aname = String.Empty;
+        private double cost;
+        private string cname = String.Empty;
+        private string citizenship = String.Empty;
+        private bool status;
     }
 }

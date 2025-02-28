34cdc02c93ed7fbdc3c362417486fbc3bbc1cb5e
Estefano Campana <Estefano.CampanaMunoz@edu.sait.ca>
2025-02-28 09:01:57 -0700

    Add project files.

---
+++ Data/Reservations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHybridApp.Data
+{
+    public class Reservations
+    {
+        //Private data
+        private string rcode = String.Empty;
+    }
+}
